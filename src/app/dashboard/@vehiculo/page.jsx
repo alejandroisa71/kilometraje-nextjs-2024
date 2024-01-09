d155cf24212f4b7b464f500422d916c482f96312
Tucuman <alejandoisa71@.gmail.com>
2024-01-09 20:46:32 -0300

    Modify list vehiculos

--- src/app/dashboard/@vehiculo/page.jsx
+++ src/app/dashboard/@vehiculo/page.jsx
@@ -1,19 +1,22 @@
+import {prisma} from '@/libs/prisma'
 import VehiculoCard from "./components/VehiculoCard"
 import VehiculoForm from "./components/VehiculoForm"
 
-const VehiculoPage =async () => {
- const response = await fetch('http://localhost:3000/api/vehiculo')
-//  console.log(response)
- const data = await response.json()
-//  console.log(data)
-//  console.log(data)
- const results = data
-//  console.log(results)
+
+async function loadVehiculo () {
+  
+  return await prisma.vehiculo.findMany()
+}
+
+const VehiculoPage = async () => {
+ 
+  const results = await loadVehiculo()
+
       
   return (
     <main>
       <h1 className="text-3xl">Vehiculo Page</h1>
-      {results?.map((result) => (
+      {results.map((result) => (
        <VehiculoCard key={result.id} result={result}/>
       ))}
       <VehiculoForm/>
