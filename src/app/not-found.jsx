77a4710b6ead5ac484895c054ea69ba70d7f0e3b
Tucuman <alejandoisa71@.gmail.com>
2024-01-10 20:10:38 -0300

    Change styled page not-found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link"
 
-const Notfound = () => {
+const NotFound = () => {
   return (
-      <div className=" flex bg-red-400 flex-1 flex-col w-full h-screen items-center justify-evenly gap-4 p-4 ">
-          <h2>404 (Page noat Found)</h2>
-          <Link href="/dashboard" >Back to Dashboard</Link>
-      </div>   
+      <section className=" flex bg-red-400 h-screen justify-center items-center">
+         <div className="text-center">
+         <h1 className="text-4xl font-bold">404 (Page Not Found)</h1>
+          <Link href="/dashboard" className="text-slate-800 hover:text-slate-500 text-2xl mt-5" >Back to Dashboard</Link>
+         </div>
+      </section>   
     )
 }
-export default Notfound
+export default NotFound
